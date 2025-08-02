c9e19213f62c2ce10e0717da78ca9de2a2ecd34c
Mahmud394 <unmahmud@gmail.com>
2025-08-03 00:12:07 +0600

    Environment Variable Added

--- tasks/models.py
+++ tasks/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.db.models.signals import post_save, pre_save, post_delete, pre_delete, m2m_changed
+from django.dispatch import receiver
+from django.core.mail import send_mail
+
 
 # Create your models here.
 
@@ -22,7 +26,7 @@ class Task(models.Model):
     #  Many to one 
     project = models.ForeignKey(
         "Project", 
-        on_delete=models.CASCADE, 
+        on_delete=models.DO_NOTHING, 
         default=1
         )
     
@@ -74,4 +78,56 @@ class Project(models.Model):
         return self.name
     
 
+# signals
 
+# @receiver(post_save, sender=Task) # post_save
+# def notify_task_creation(sender, instance, created, **kawrgs):
+#     print('sender', sender)
+#     print('instance', instance)
+#     print(kawrgs)
+#     print(created)
+#     if created:
+#         instance.is_completed = True
+#         instance.save()
+
+
+# @receiver(pre_save, sender=Task) # pre_save
+# def notify_task_creation(sender, instance, **kawrgs):
+#     print('sender', sender)
+#     print('instance', instance)
+#     print(kawrgs)
+    
+#     instance.is_completed = True
+
+
+
+# @receiver(pre_delete, sender=Task) # pre_delete
+# def notify_task_creation(sender, instance, **kawrgs):
+#     print('sender', sender)
+#     print('instance', instance)
+#     print(kawrgs)
+
+
+@receiver(m2m_changed, sender=Task.assigned_to.through) # many to many
+def notify_task_creation(sender, instance, action, **kawrgs):
+    if action =='post_add':
+        assigned_emails = [ emp.email for emp in instance.assigned_to.all()]
+        
+        send_mail(
+            "New Task Assigned",
+            f"You have been assigned to the task: {instance.title}.",
+            "unmahmud@gmail.com",
+            assigned_emails,
+            fail_silently=False,
+        )
+        
+@receiver(post_delete, sender=Task) # post_delete
+def delete_associated_details(sender, instance, **kawrgs):
+    if instance.details:
+        print('instance', instance)
+        instance.details.delete()
+        
+        print("deleted successfully")
+   
+    
+    
